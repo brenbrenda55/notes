bf3ac540c14248e34f2055441a5a7ca99a7fe538
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-09-27 20:06:00 -0500

    finished

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-3.06 KiB
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notes } = require('./db/db.json')
+let  notes  = require('./db/db.json')
+const { v4: uuidv4 } = require('uuid');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,67 +18,59 @@ function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
     fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify({ notes: notessArray }, null, 2)
-    );
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray) 
+        );
     return note;
-  };
+};
 
 
-  app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     let results = notes;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
-  
-  app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-  });
-  
-  app.post('/api/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notess.length.toString();
-  
-    if (!validateAnimal(req.body)) {
-      res.status(400).send('The note is not properly formatted.');
-    } else {
-      const note = createNewnote(req.body, notes);
-      res.json(animal);
-    }
-  });
-  
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-  
-  
-
-  app.delete('api/notes/:id', (req, res) => {
     
-  }
-  )
-  
+    res.json(results);
+});
 
 
 
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    console.log(req.body)
+    req.body.id = uuidv4();
+    console.log(req.body)
+
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    
+});
 
 
+
+app.delete('/api/notes/:id', (req, res) => {
+    console.log (req.params.id)
+    notes = notes.filter (note => note.id !== req.params.id )
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notes) 
+        );
+
+        res.json(notes)
+}
+)
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 
 
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
-  
+});
